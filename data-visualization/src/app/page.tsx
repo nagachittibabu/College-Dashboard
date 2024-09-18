462ef0c2bd5452db18b2b083c2ae0b2eeb036efd
venlancer <venlancer@gmail.com>
2024-09-18 19:16:40 +0530

    css rendering changes

--- data-visualization/src/app/page.tsx
+++ data-visualization/src/app/page.tsx
@@ -1,8 +1,14 @@
 import Image from "next/image";
 import DashboardHome from "./DashboardApp/page";
+import { ConfigProvider } from 'antd';
+import 'antd/dist/reset.css'; // Import reset CSS from Ant Design 5
+import '../app/globals.css'; // Your global styles
 
 export default function Home() {
   return (
-<DashboardHome></DashboardHome>
+    <ConfigProvider theme={{ token: { colorPrimary: '#1890ff' } }}>
+    <DashboardHome></DashboardHome>
+  </ConfigProvider>
+
   );
 }
