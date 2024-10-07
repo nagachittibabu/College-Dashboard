115585be7103a522da7734ab66b378d2923738f5
ASSPVReddy <136595740+ASSPVReddy@users.noreply.github.com>
2024-10-07 18:43:06 +0530

    SelectedProfessor folder added

--- data-visualization/src/app/ProfessorDetails/page.tsx
+++ data-visualization/src/app/ProfessorDetails/page.tsx
@@ -1,13 +1,15 @@
 'use client';
+import { usePathname } from 'next/navigation';
 import React, { useEffect, useState } from 'react';
 
 const ProfessorDetails = () => {
-    const [details, setDetail] = useState([]);
+    const [details, setDetail] = useState();
+    const selectedVal = usePathname().split('/')[2];
     useEffect(() => {
         fetch('/professors.json')
             .then((response) => response.json())
-            .then((data) => { 
-                setDetail(data[0]);
+            .then((data) => {
+                setDetail(data[selectedVal]);
             });
     }, []);
         
@@ -15,7 +17,7 @@ const ProfessorDetails = () => {
         <div className="p-6 mx-auto">
             {details && ( 
             <div className="flex gap-7">
-                <div className="w-2/5 shadow-md px-3 rounded-xl">
+                <div className="w-2/5 border shadow-md px-3 rounded-xl">
                     <div className="mb-5">
                         <div className="p-6">
                             <div className="flex justify-center mb-5">
@@ -27,27 +29,28 @@ const ProfessorDetails = () => {
                                 <p className="my-5">{details.address}</p>
                             </div>
                             <div className="flex justify-center">
-                                <button className="bg-purple-600 rounded-full px-4 py-2 text-white mr-2">Follow</button>
-                                <button className="bg-purple-200 rounded-full px-5 py-2 text-purple-600">Message</button>
+                                <button className="bg-orange-500 rounded-full px-4 py-2 text-white mr-2">Follow</button>
+                                <button className="bg-orange-200 rounded-full px-5 py-2 text-orange-600">Message</button>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="w-3/5 shadow-md px-3 rounded-xl">
+                <div className="w-3/5 border shadow-md px-3 rounded-xl">
                     <div className="p-5">
-                        <ul className="flex">
-                            <li><a href="#">About</a></li>
-                            <li><a href="#">Account</a></li>
-                        </ul>
-                        <div className="">
-                            <div className="py-30">
-                                <ul>
-                                    <li><p><i className="fa-solid fa-graduation-cap me-1"></i><strong>Degree:</strong> {details.qualification}</p></li>
-                                    <li><p><i className="fa-solid fa-star me-1"></i><strong>Experience:</strong> {details.experience}</p></li>
-                                    <li><p><i className="fa-solid fa-right-long me-1"></i><strong>My Courses:</strong> Higher Math, Math Puzzle</p></li>
-                                    <li><p><i className="fa-solid fa-right-long me-1"></i><strong>Projects:</strong> Map Creation</p></li>
-                                </ul>
-                                <hr />
+                        <div className="flex">
+                            <div className='border bg-orange-500 text-white py-2 px-4'><a href="#">About</a></div>
+                            <div className='border py-2 px-4'><a href="#">Account</a></div>
+                        </div>
+                        <hr />
+                        <div className="py-5">
+                            <div className="leading-9 pb-3">
+                                <p><i className="fa-solid fa-graduation-cap me-1"></i><strong>Degree:</strong> {details.qualification}</p>
+                                <p><i className="fa-solid fa-star me-1"></i><strong>Experience:</strong> {details.experience}</p>
+                                <p><i className="fa-solid fa-right-long me-1"></i><strong>Subjects:</strong> Higher Math, Math Puzzle</p>
+                                <p><i className="fa-solid fa-right-long me-1"></i><strong>Projects:</strong> Map Creation</p>
+                            </div>
+                            <hr />
+                            <div className='pt-4'>
                                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas in finibus neque. Vivamus in ipsum quis elit vehicula tempus vitae quis lacus. Vestibulum interdum diam non mi cursus venenatis. Morbi lacinia libero et elementum vulputate. Vivamus et facilisis mauris. Maecenas nec massa auctor, ultricies massa eu, tristique erat. Vivamus in ipsum quis elit vehicula tempus vitae quis lacus. Eu pellentesque, accumsan tellus leo, ultrices mi dui lectus sem nulla eu.Eu pellentesque, accumsan tellus leo, ultrices mi dui</p>
                             </div>
                         </div>
--- data-visualization/src/app/ProfessorsList/page.tsx
+++ data-visualization/src/app/ProfessorsList/page.tsx
@@ -1,9 +1,17 @@
+'use client';
+import React from 'react';
 import Header from "../header/page";
 import Sidenav from "../sideNav/page";
 import Professorcard from "../ProfessorCard/page";
 import ProfessorsData from "../../../public/professors.json";
+import { useRouter } from 'next/navigation';
 
 const ProfessorsList = () => {
+    const router = useRouter();
+    const handleCardClick = (item:any) => {
+        router.push(`/ProfessorDetails/${item.key}`)
+    };
+    
     return (
         <>
         <div>
@@ -15,6 +23,7 @@ const ProfessorsList = () => {
         <div className="content-area flex flex-wrap justify-center gap-12 pt-32">
             {ProfessorsData.map((item) => {
                 return (
+                    <div onClick={() => handleCardClick(item)}>
                     <Professorcard
                         Name={item.name}
                         Role={item.role}
@@ -22,7 +31,9 @@ const ProfessorsList = () => {
                         Address={item.address}
                         Mobile={item.mobileNumber}
                     />
+                    </div>
                 );
+                
             })}
         </div>
         </>
