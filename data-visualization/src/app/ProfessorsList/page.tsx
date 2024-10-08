df08fb3e4929642c7b130a3b4417568b7283d34c
ASSPVReddy <136595740+ASSPVReddy@users.noreply.github.com>
2024-10-08 13:58:28 +0530

    navigation added

--- data-visualization/src/app/ProfessorsList/page.tsx
+++ data-visualization/src/app/ProfessorsList/page.tsx
@@ -20,10 +20,13 @@ const ProfessorsList = () => {
         <div className='sidenav-container'>
             <Sidenav />
         </div>
-        <div className="content-area flex flex-wrap justify-center gap-12 pt-32">
+        <div className='flex pt-24 pl-40 text-xl'>
+            <a href="/">Home </a>
+        </div>
+        <div className="content-area flex flex-wrap justify-center gap-12 pt-10">
             {ProfessorsData.map((item) => {
                 return (
-                    <div onClick={() => handleCardClick(item)}>
+                    <div onClick={() => handleCardClick(item)} className='cursor-pointer'>
                     <Professorcard
                         Name={item.name}
                         Role={item.role}
